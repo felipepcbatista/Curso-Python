9e3c749499611879df69a3e35ebec15c6b53c61f
Felipe Paz Carvalho Batista <lipepaz23@gmail.com>
2025-08-19 13:46:57 -0300

    Exercicio 6 - done

---
+++ exercicio6.py
@@ -0,0 +1,14 @@
+senha_correta = "python123"
+tentativas = 0
+while tentativas < 3:
+    senha = str(input("Digite a senha de acesso: "))
+    tentativas += 1
+    if senha == senha_correta:
+        print("Bem-vindo!")
+        break
+    else:
+        if tentativas == 3:
+            print("Conta bloqueada.")
+            break
+        else:
+            print(f"Senha incorreta. Você tem mais {3 - tentativas} tentativa(s).")
